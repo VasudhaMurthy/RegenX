d751c5e406a5e4d9fbe9a25f5229c691bbbd7082
Pragna <pragnauk1@gmail.com>
2025-08-09 13:30:47 +0530

    Grievance Errors cleared.

---
+++ app/src/main/java/com/example/regenx/model/Grievance.kt
@@ -0,0 +1,8 @@
+package com.example.regenx.model
+data class Grievance(
+    val id: String = "",
+    val type: String = "",
+    val userId: String = "",
+    val description: String = "",
+    val status: String = ""
+)
