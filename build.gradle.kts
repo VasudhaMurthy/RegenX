a1e65a447b7e14a0938afc665f31ef6f1996ed3c
Moulya <mona21vishu@gmail.com>
2025-11-25 23:45:04 +0530

    scrap collector complete

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    id("com.android.application") version "8.9.2" apply false
+    // Keep only the highest version for the Android Application plugin
     id("com.android.application") version "8.13.1" apply false
+
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false
